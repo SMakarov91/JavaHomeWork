f3230779b98252e896b5725c45829cee8e25ecf0
Sergey Makarov <smakarovprivat@gmail.com>
2021-10-23 18:00:25 +0300

    commit

---
+++ src/com/pb/makarov/hw4/CapitalLetter.java
@@ -0,0 +1,31 @@
+package com.pb.makarov.hw4;
+
+import javafx.beans.binding.StringBinding;
+
+import java.util.Scanner;
+
+public class CapitalLetter {
+
+    static String scanStr() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = scan.nextLine();
+
+        return str;
+    }
+
+    public static String toCamelCase(String givStr) {
+        String[] arr = givStr.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0))).append(arr[i].substring(1)).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toCamelCase(scanStr()));
+    }
+}
